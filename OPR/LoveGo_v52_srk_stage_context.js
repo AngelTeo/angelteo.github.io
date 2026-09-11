1673a2d0b463ec5480fbceb28de4bb990283a4f0
AngelTeo <teo.beefong@hotmail.com>
2026-09-12 01:13:03 +0800

    LoveGo v58: load SRK runtime QC

--- OPR/LoveGo_v52_srk_stage_context.js
+++ OPR/LoveGo_v52_srk_stage_context.js
@@ -52,4 +52,5 @@
   loadOnce('LoveGo_v54_srk_registry_adapter.js','lovego-v54-srk-adapter');
   loadOnce('LoveGo_v55_srk_cloud_guard.js','lovego-v55-srk-cloud-guard');
   loadOnce('LoveGo_v57_srk_registry_semantic_guard.js','lovego-v57-srk-semantic');
+  loadOnce('LoveGo_v58_srk_runtime_qc.js','lovego-v58-srk-runtime-qc');
 })();
